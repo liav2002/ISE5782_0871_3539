9a46de5b1709271c58c4e887db7d1d1a4974c2a4
Eyal <seyal613@gmail.com>
2022-03-13 16:18:26 +0200

    Cylinder test -> implement Cylinder getNormal -> implement

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -13,8 +13,15 @@ public class Cylinder extends Tube {
     }
 
     @Override
-    public Vector getNormal(Point p) {
-        return null;
+    public Vector getNormal(Point pnt) {
+        Point topBaseCenter = this.axisRay.getPoint(this.height);//the center of the top base of the cylinder
+        //if the point is on the bottom base make plane normal calculation:
+        if (pnt.length(this.axisRay.getP0()) <= this.radius) {//only on the base the distance between the center of the cylinder and the point is less then the radius, according to the Triangle inequality rule.
+            return this.axisRay.getDir().normalize().scale(-1);//the normal is the ray direction*-1 because we want to ger outside of the cylinder
+        } else if (pnt.length(topBaseCenter) <= this.radius) {//the same calculation as before, just for the top base
+            return this.axisRay.getDir();
+        }
+        return super.getNormal(pnt);
     }
 
     public double getHeight() {
--- src/unittest/geometries/CylinderTest.java
+++ src/unittest/geometries/CylinderTest.java
@@ -1,5 +1,6 @@
 package unittest.geometries;
 
+import geometries.Cylinder;
 import org.junit.Test;
 import primitives.Point;
 import primitives.Ray;
@@ -12,6 +13,7 @@ import static org.junit.Assert.*;
 
 /**
  * Unit tests for geometries.Cylinder class
+ *
  * @author1 Eyal
  * @authon2 Liav Ariel
  */
@@ -22,8 +24,24 @@ public class CylinderTest {
 
     }
 
+    /**
+     * Test method for {@link Cylinder#getNormal(Point)}.
+     */
     @Test
     public void testGetNormal() {
+        Cylinder c = new Cylinder(1, 1, new Ray(Point.ZERO, new Vector(0, 1, 0)));
+        //TC01: check the Tube getNormal for דםצק extreme cases there
 
+        //TC02: the point on the side of the cylinder
+        assertEquals("getNormal() bad normal on the side of the cylinder",
+                c.getNormal(new Point(0, 0.5, 1)), new Vector(0, 0, 1));
+
+        //TC03: the point on the top base of teh cylinder
+        assertEquals("getNormal() bad normal on the top base of the cylinder",
+                c.getNormal(new Point(0.1, 1, 0.1)), new Vector(0, 1, 0));
+
+        //TC04: the point on the bottom base of the cylinder
+        assertEquals("getNormal() bad normal on the bottom base of the cylinder",
+                c.getNormal(new Point(0.1, 0, 0.1)), new Vector(0, -1, 0));
     }
 }
