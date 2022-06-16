6aec132cd4459ecc025a9b7cbe91192f9e485391
Eyal <seyal613@gmail.com>
2022-06-16 18:06:03 +0300

    final changes

--- src/com/ISE/Main.java
+++ src/com/ISE/Main.java
@@ -1,11 +1,4 @@
-/*
- *  Mini Project in Introduction to Software Engineering.
- *
- *  @author1  Liav Ariel (212830871), halkadar@g.jct.ac.il
- *  @author2  Eyal Seckbach (324863539), seyal613@gmail.com
- *
- *  Lecture: Yair Goldstein.
- */
+
 
 package com.ISE;
 
